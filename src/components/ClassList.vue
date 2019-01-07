ea7cd11bd978013188206f97f140c0eff30e608c
Jason Ruggles <jason.ruggles@hotmail.com>
2019-01-07 13:07:17 -0500

    Initial commit

---
+++ src/components/ClassList.vue
@@ -0,0 +1,103 @@
+<template>
+	<v-container>
+		<v-layout row wrap>
+			<v-flex xs12 sm6 offset-sm3>
+				<div class="grey--text text-uppercase text-xs-right pr-1">
+					Class List
+				</div>
+				<v-card>
+					<v-container fluid>
+						<v-layout row wrap>
+							<v-flex v-for="(item) in classes" :key="item.name" xs6 sm6 md4 lg4 xl3 class="pa-1">
+								
+									<v-btn large block round outline color="teal darken-1" :disabled="!item.active" >
+										<span class="title" >{{item.name}}</span>
+									</v-btn>
+								
+							</v-flex>
+						</v-layout>
+					</v-container>
+
+
+
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		data () {
+			return { 
+				classes: [
+					{
+						name: "AS",
+						description: "A Stock",
+						active: true,
+					},
+					{
+						name: "ASL",
+						description: "A Stock Ladies",
+						active: false
+					},
+					{
+						name: "ASP",
+						description: "A Street Prepared",
+						active: true,
+					},
+					{
+						name: "ASPL",
+						description: "A Street Prepared Ladies",
+						active: true,
+					},
+					{
+						name: "BS",
+						description: "A Stock",
+						active: true,
+					},
+					{
+						name: "BSL",
+						description: "B Stock",
+						active: true,
+					},
+					{
+						name: "BSP",
+						description: "A Stock",
+						active: true,
+					},
+					{
+						name: "BSPL",
+						description: "B Stock",
+						active: true,
+					},
+					{
+						name: "BP",
+						description: "A Stock"
+					},
+					{
+						name: "BPL",
+						description: "B Stock"
+					},
+					{
+						name: "CS",
+						description: "A Stock"
+					},
+					{
+						name: "CSL",
+						description: "B Stock"
+					},
+					{
+						name: "CSP",
+						description: "A Stock"
+					},
+					{
+						name: "CSPL",
+						description: "B Stock"
+					},
+
+				]
+			}
+		}
+	}
+</script>
---
+++ src/components/RunTicker.vue
@@ -0,0 +1,101 @@
+<template>
+	<v-container>
+		<v-layout row>
+			<v-flex xs12 sm6 offset-sm3>
+				<div class="grey--text text-uppercase text-xs-right pr-1">
+					Run Ticker
+				</div>
+				<v-card>
+					<v-list two-line>
+						<template v-for="(run, index) in runs">
+							<v-list-tile :key="run.id">
+								<v-list-tile-content>
+									<v-list-tile-title>
+										<span class="grey--text text--darken-3">{{run.carnumber}}</span>
+										&nbsp;-&nbsp;
+										<span class="grey--text text--darken-1">{{run.driver}}</span>
+									</v-list-tile-title>
+									<v-list-tile-sub-title>{{run.runtime}} - Run #{{run.runnumber}}</v-list-tile-sub-title>
+								</v-list-tile-content>
+								<v-list-tile-action>
+									<v-list-tile-action-text class="title grey--text text--darken-2 font-weight-medium">{{run.runresult}}</v-list-tile-action-text>
+								</v-list-tile-action>
+							</v-list-tile>
+							<v-divider v-if="index + 1 < runs.length" :key="index"></v-divider>
+						</template>
+					</v-list>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+
+
+<script>
+	export default {
+		data () {
+			return { 
+				runs: [
+					{
+						id: "14",
+						carnumber: "197 AS",
+						driver: "Jim Fossum",
+						runnumber: 3,
+						runresult: "51.382",
+						runtime: "3 Mins Ago"
+					},
+					{
+						id: "13",
+						carnumber: "97 AS",
+						driver: "Jason Ruggles",
+						runnumber: 3,
+						runresult: "49.352",
+						runtime: "4 Mins Ago"
+					},
+					{
+						id: "12",
+						carnumber: "197 AS",
+						driver: "Jim Fossum",
+						runnumber: 2,
+						runresult: "48.092 + 1",
+						runtime: "4 Mins Ago"
+					},
+					{
+						id: "11",
+						carnumber: "97 AS",
+						driver: "Jason Ruggles",
+						runnumber: 2,
+						runresult: "47.866 + 1",
+						runtime: "6 Mins Ago"
+					},
+					{
+						id: "10",
+						carnumber: "197 AS",
+						driver: "Jim Fossum",
+						runnumber: 9,
+						runresult: "49.352",
+						runtime: "4 Mins Ago"
+					},
+					{
+						id: "9",
+						carnumber: "97 AS",
+						driver: "Jason Ruggles",
+						runnumber: 10,
+						runresult: "47.352 + 1",
+						runtime: "3 Mins Ago"
+					},
+					{
+						id: "8",
+						carnumber: "197 AS",
+						driver: "Jim Fossum",
+						runnumber: 9,
+						runresult: "49.352",
+						runtime: "4 Mins Ago"
+					},
+
+				]
+			}
+		}
+	}
+</script>
